7652b8b4774df30608563f2700ae69299951e9a5
Dan Kimberley <danielkimberley@gmail.com>
2026-02-16 18:30:54 +0000

    displaying distances from the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs/promises'
 
 
 const app = express();
@@ -10,6 +11,19 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from the backend'});
 });
+
+app.get('/api/activities', async (req, res) => {
+    try{
+        const data = await fs.readFile('./data/activities.json', 'utf-8')
+        const activities = JSON.parse(data)
+        res.json(activities)
+    } catch {
+        res.status(500).json({error: 'Failed to read activities',})
+    }
+    
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
+type Activity = {
+  id: number;
+  date: string;
+  type: string;
+  distance: number;
+  duration: number;
+  elevation_gain: number;
+  average_heart_rate: number;
+  average_speed: number;
+};
+
 function App() {
   const [message, setMessage] = useState<string>("");
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:3000")
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error("Error:", error));
-  });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/activities")
+      .then((response) => response.json())
+      .then((data) => setActivities(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  const getChartData = (activities: Activity[]) => {
+    return activities.map(activity => ({
+      date: activity.date,
+      distance: activity.distance
+    }))
+  }
 
   return (
     <>
       <div>
         <h1>Fitness tracker</h1>
         <p>{message}</p>
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>
+              {activity.date} - {activity.type} - {activity.distance}km
+            </li>
+          ))}
+        </ul>
+        <p>{JSON.stringify(getChartData(activities))}</p>
       </div>
     </>
   );
